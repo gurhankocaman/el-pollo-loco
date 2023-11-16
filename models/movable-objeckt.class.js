b86bc212b1468074e7e686e9a8f35e0774e84ba3
Gürhan Kocaman <gurhankocaman@web.de>
2023-11-16 23:25:04 +0100

    firstSave

---
+++ models/movable-objeckt.class.js
@@ -0,0 +1,22 @@
+class MovableObject {
+        x = 120;
+        y = 250;
+        img;
+        height = 150;
+        width = 100; 
+
+
+        loadImage(path) {
+            this.img = new Image(); // this.img = document.getElementById('image'); yani <img id="image" src>
+            this.img.src = path;
+        }
+
+        moveRight() {
+            console.log('Moving right');
+        }
+
+        moveLeft(){
+        
+        }
+}
+
---
+++ models/world.class.js
@@ -0,0 +1,57 @@
+class World {
+    character = new Character();
+    enemies = [
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+    ];
+    clouds = [
+        new Cloud()
+    ];
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png')
+    ];
+    canvas;
+    ctx;
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+    }
+
+
+
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.addToMap(this.character);
+
+        this.enemies.forEach(enemy => {
+            this.addToMap(enemy);
+        });
+
+
+        this.clouds.forEach(cloud => {
+            this.addToMap(cloud);
+        });
+
+        this.backgroundObjects.forEach((bgo) => {
+            this.addToMap(bgo);
+        });
+
+        // draw() wird immer wieder aufgerufen
+        let self = this;   // resimi sürekli göstermesi icin
+        requestAnimationFrame(function(){
+            self.draw();
+        });
+    }
+
+
+
+    
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+    }
+}
