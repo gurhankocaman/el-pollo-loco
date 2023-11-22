289c20b191615b3ed2af84e41b4bba8ce99a54cb
Gürhan Kocaman <gurhankocaman@web.de>
2023-11-22 01:05:12 +0100

    newBackgroundObjects

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,8 @@
 class Character extends MovableObject {
-
-
+    y = 155;
+    height = 275;
+    width = 120; 
+    
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
     }
