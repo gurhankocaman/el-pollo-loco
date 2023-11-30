434af514a6a1c9cce1ca4bf1b5bbcffa2f0193aa
Gürhan Kocaman <gurhankocaman@web.de>
2023-12-01 00:07:41 +0100

    characterMove

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject {
     y = 155;
     height = 275;
     width = 120;
+    speed = 10;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -10,7 +11,7 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png'
     ];
-    currentImage = 0; 
+    world;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
@@ -20,12 +21,34 @@ class Character extends MovableObject {
     }
 
     animate() {
+
         setInterval(() => {
-            let i = this.currentImage % this.IMAGES_WALKING.length; // i = 0, 1, 2, 3, 4, 5, 6, und dann wieder 0..
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++; 
-        }, 100);
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 60);
+
+
+
+
+
+
+
+        setInterval(() => {
+
+            if (this.world.keyboard.RIGHT ||this.world.keyboard.LEFT) {
+                // Walk animation
+                let i = this.currentImage % this.IMAGES_WALKING.length; // i = 0, 1, 2, 3, 4, 5, 6, und dann wieder 0..
+                let path = this.IMAGES_WALKING[i];
+                this.img = this.imageCache[path];
+                this.currentImage++; 
+            }
+
+
+        }, 50);
 
     }
 
